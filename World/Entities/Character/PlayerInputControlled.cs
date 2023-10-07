e855c2580a61ac904609f9208bfbdb3b4812caf6
Duncan Saunders <titch007uk@hotmail.com>
2023-10-06 17:07:44 -0700

    updated animation controller and added new sprites for prototyping walls.

--- World/Entities/Character/PlayerInputControlled.cs
+++ World/Entities/Character/PlayerInputControlled.cs
@@ -1,46 +1,24 @@
 using Godot;
 
+namespace SpookyBotanyGame.World.Entities.Character;
+
 public partial class PlayerInputControlled : CharacterBody2D
 {
-
     [Export] public float Speed { get; set; } = 400;
-    [Export] private AnimationPlayer Animation { get; set; }
-
-    private string _animationDirection = "down";
+    [Export] private FourDirectionAnimationController Animation { get; set; }
 
     public void GetInput()
     {
         Vector2 inputDirection = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-        if (inputDirection.X > 0)
-        {
-            _animationDirection = "right";
-        }
-
-        if (inputDirection.X < 0)
-        {
-            _animationDirection = "left";
-        }
-
-        if (inputDirection.Y > 0)
-        {
-            _animationDirection = "down";
-        }
-
-        if (inputDirection.Y < 0)
-        {
-            _animationDirection = "up";
-        }
         Velocity = inputDirection * Speed;
-
+        
         if (inputDirection == Vector2.Zero)
         {
-            Animation.Play(_animationDirection + "_idle");
-            Animation.Advance(0);
+            Animation.Play(inputDirection,"idle");
         }
         else
         {
-            Animation.Play(_animationDirection + "_walk");
-            Animation.Advance(0);
+            Animation.Play(inputDirection, "walk");
         }
     }
     
